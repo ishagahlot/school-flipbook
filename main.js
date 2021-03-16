b96d898f50423a35407cd301024e781fe735e23b
ishagahlot <65116653+ishagahlot@users.noreply.github.com>
2021-03-17 00:16:15 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+const html = document.documentElement;
+const canvas = document.querySelector('.school');
+const context = canvas.getContext("2d");
+
+const frameCount = 37;
+
+const currentFrame = index => (
+  `./img/${index.toString().padStart(2, '0')}.jpg`
+)
+
+const preloadImages = () => {
+  for (let i = 1; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+  }
+};
+
+const img = new Image()
+img.src = currentFrame(1);
+canvas.width=1920;
+canvas.height=1280;
+img.onload=function(){
+  context.drawImage(img, 0, 0);
+}
+
+const updateImage = index => {
+  img.src = currentFrame(index);
+  context.drawImage(img, 0, 0);
+}
+
+window.addEventListener('scroll', () => {  
+  const scrollTop = html.scrollTop;
+  const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const scrollFraction = scrollTop / maxScrollTop;
+  const frameIndex = Math.min(
+    frameCount - 1,
+    Math.floor(scrollFraction * frameCount)
+  );
+  
+  requestAnim
